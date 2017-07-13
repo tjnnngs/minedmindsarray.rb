98804304e4683cb1e6647c5b559952399770197e
TONYA JENNINGS <tjennings@kcs059702-tl.hitronhub.home>
2017-07-13 11:06:19 -0400

    mined minds array test 1

--- minedmindsarray_test.rb
+++ minedmindsarray_test.rb
@@ -1,1 +1,15 @@
-minedmindsarray_test.rb
+
+require "minitest/autorun"
+require_relative "minedmindsarray.rb"
+
+class TestMindedMindsArray < Minitest::Test
+
+	def test_assrt_that_1_equals_1
+		assert_equal(1, 1)
+	end
+	def test_output_is_an_array
+		mm_array = create_mined_minds_array()
+		assert_equal(Array, mm_array.class)
+	end
+
+end
