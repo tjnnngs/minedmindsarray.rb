728d5f35aa6f5523779adfde20aa35d41afe94c5
TONYA JENNINGS <tjennings@kcs059702-tl.hitronhub.home>
2017-07-13 14:06:59 -0400

    array with one counter line

--- minedmindsarray.rb
+++ minedmindsarray.rb
@@ -4,17 +4,18 @@ def create_mined_minds_array()
 	100.times do
 		if  counter % 15 == 0
 			my_arr.push("minedminds")
-			counter = counter + 1
+			
 		elsif counter % 3 == 0
 			my_arr.push("mined")
-			counter = counter + 1
+			
 		elsif counter % 5 == 0
 			my_arr.push("minds")
-			counter = counter + 1	
+			
 		else 
 		my_arr.push(counter)
-		counter = counter + 1
+		
 		end
+		counter = counter + 1
 	end
 	p my_arr
 end
