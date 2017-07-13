75f0527b738d28920dddfa992290320132e504ee
TONYA JENNINGS <tjennings@kcs059702-tl.hitronhub.home>
2017-07-13 14:06:02 -0400

    array with multiple counters

--- minedmindsarray.rb
+++ minedmindsarray.rb
@@ -1,3 +1,20 @@
 def create_mined_minds_array()
-	[]
+	counter = 1
+	my_arr = []
+	100.times do
+		if  counter % 15 == 0
+			my_arr.push("minedminds")
+			counter = counter + 1
+		elsif counter % 3 == 0
+			my_arr.push("mined")
+			counter = counter + 1
+		elsif counter % 5 == 0
+			my_arr.push("minds")
+			counter = counter + 1	
+		else 
+		my_arr.push(counter)
+		counter = counter + 1
+		end
+	end
+	p my_arr
 end
--- minedmindsarray_test.rb
+++ minedmindsarray_test.rb
@@ -7,9 +7,41 @@ class TestMindedMindsArray < Minitest::Test
 	def test_assrt_that_1_equals_1
 		assert_equal(1, 1)
 	end
+
 	def test_output_is_an_array
 		mm_array = create_mined_minds_array()
 		assert_equal(Array, mm_array.class)
 	end
 
+	def test_array_length_is_100
+		mm_array = create_mined_minds_array()
+		assert_equal(100, mm_array.length)
+	end
+	
+	p create_mined_minds_array
+
+
+	def test_zero_position_in_array_is_one
+		mm_array = create_mined_minds_array()
+		assert_equal(1, mm_array[0])
+		
+	end
+
+	def second_position_in_array_is_mined
+		mm_array = create_mined_minds_array()
+		assert_equal("mined", mm_array[2])
+	end
+
+	def test_fifth_postion_in_array_is_mined
+		mm_array = create_mined_minds_array()
+		assert_equal("minds", mm_array[4])
+	end
+	
+	def test_all_numbers_divisible_15_prints_minedminds
+		mm_array = create_mined_minds_array()
+		assert_equal("minedminds", mm_array[14])
+	end	
 end
+	
+ 
+
